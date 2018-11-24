9fe2211c11c202bff01739aff766f7543752eebd
Jerry Ho Nai Yi <jerry.ho@qinetics.local>
2018-11-25 03:28:29 +0800

    bug fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8000;
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Body parser middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Setup route
@@ -19,14 +19,14 @@ let router = express.Router();
 app.use('/api', router);
 
 // Allow cross origin requests
-const cors = require('cors')({origin:true});
+const cors = require('cors')({ origin: true });
 app.use(cors);
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.json({ message: 'API is working!' });
 });
 
-router.post('/sendEmail', function(req, res) {
+router.post('/sendEmail', function (req, res) {
     let email = req.body.email;
     let name = req.body.name;
     let message = `
@@ -38,37 +38,36 @@ router.post('/sendEmail', function(req, res) {
         <p>${req.body.message}</p>
     `;
 
-    nodemailer.createTestAccount(function(err, account) {
-        let transporter = nodemailer.createTransport({
-            host: process.env.HOST, //'smtp.gmail.com'
-            port: process.env.EPORT, //587
-            secure: false,
-            auth: {
-                user: process.env.USER, //'youremail@gmail.com',
-                pass: process.env.PASS //'yourpassword'
-            },
-            tls: {
-                rejectUnauthorized: false
-            }
-        });
+    var transporter = nodemailer.createTransport({
+        host: process.env.HOST, //'smtp.gmail.com'
+        port: process.env.EPORT, //587
+        secure: false,
+        auth: {
+            user: process.env.USER, //'youremail@gmail.com',
+            pass: process.env.PASS //'yourpassword'
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
 
-        let mailOptions = {
-            from: `${req.body.name} <${req.body.email}>`, // Sender address
-            to: 'huiteng1995@gmail.com', // List of receivers
-            subject: 'Contact Request', // Subject line
-            text: '', // Plain text body
-            html: message // Html body
-        };
+    let mailOptions = {
+        from: `${process.env.USER} <${process.env.USER}>`, // Can't use Sender address because you are using ur own gmail
+        to: 'huiteng1995@gmail.com', // List of receivers
+        subject: `Contact Request from ${req.body.email}`, // Subject line
+        text: message, // Plain text body
+        html: message // Html body
+    };
 
-        transporter.sendMail(mailOptions,function(error,info) {
-            if(error){
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-            res.json({message: "sending email to " + name + " at " + email});
-        });
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        res.json({ message: "sending email to " + name + " at " + email });
     });
 });
 

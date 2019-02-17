691cd84edea7f16ff9f758de9c4e0e424632a335
Hui Teng <huiteng1995@gmail.com>
2019-02-17 11:46:07 +0000

    Added animations to elements

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,4 +77,54 @@
         $('#startup-modal-container').addClass('out');
         $('body').removeClass('modal-active');
     });
+
+    // Init ScrollMagic
+    $('[data-scrollmagic]').each(function (index, elem) {
+        // Init ScrollMagic Controller
+        let controller = new ScrollMagic.Controller();
+
+        // Create Animations
+        let upTitle = $(elem).find('h1.scrollmagic-upTitle'),
+            leftSub = $(elem).find('h3.scrollmagic-leftSub'),
+            rightSub = $(elem).find('h3.scrollmagic-rightSub'),
+            upPara = $(elem).find('p.scrollmagic-upPara'),
+            leftPara = $(elem).find('p.scrollmagic-leftPara'),
+            rightPara = $(elem).find('p.scrollmagic-rightPara'),
+            upImg = $(elem).find('img.scrollmagic-upImg'),
+            leftImg = $(elem).find('img.scrollmagic-leftImg'),
+            rightImg = $(elem).find('img.scrollmagic-rightImg'),
+            upDiv = $(elem).find('div.scrollmagic-upDiv'),
+            downDiv = $(elem).find('div.scrollmagic-downDiv'),
+            upCap = $(elem).find('p.scrollmagic-upCap'),
+            leftList = $(elem).find('ul.scrollmagic-leftList'),
+            upBtn = $(elem).find('a.scrollmagic-upBtn');
+
+        let tl = new TimelineMax({pause: true});
+        tl.add("start") // add timeline label
+            .fromTo(upTitle, 0.4, { y: '40px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(leftSub, 0.5, { x: '-60px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(rightSub, 0.5, { x: '60px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(upPara, 0.5, { y: '120px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(leftPara, 0.5, { x: '-60px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(rightPara, 0.5, { x: '60px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(upImg, 0.5, { y: '60px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(leftImg, 0.5, { x: '-60px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(rightImg, 0.5, { x: '60px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(upDiv, 0.5, { y: '-40px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(downDiv, 0.5, { y: '40px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(upCap, 0.4, { y: '-20px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(leftList, 0.5, { x: '-60px', opacity: 0 }, { x: 0, opacity: 1, ease: Power2.EaseInOut }, "start")
+            .fromTo(upBtn, 0.4, { y: '40px', opacity: 0 }, { y: 0, opacity: 1, ease: Power2.EaseInOut }, "start");
+
+        // Create the Scene and trigger when visible
+        let scene = new ScrollMagic.Scene({
+            triggerElement: elem,
+            offset: -100 // offset the trigger number of px below scene's top
+        })
+            .setTween(tl)
+            .addTo(controller);
+
+        // Add debug indicators fixed on right side
+        // scene.addIndicators();
+    });
 })(jQuery);
